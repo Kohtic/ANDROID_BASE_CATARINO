9a7bcec7ed7dafc963756775d8b01f5a6ef0c1ce
hugo <huugoomoi@gmail.com>
2025-10-05 00:04:27 +0200

    test1

--- app/src/main/java/UPsay/decouverteAndroid/ToGraphic.java
+++ app/src/main/java/UPsay/decouverteAndroid/ToGraphic.java
@@ -1,18 +1,44 @@
 package UPsay.decouverteAndroid;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+import android.os.Handler;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class ToGraphic extends View {
+
+    Handler timerHandler = new Handler();
+    float xText, yText;
+    int cpt;
     public ToGraphic(Context context, @Nullable AttributeSet attrs) {
+
         super(context, attrs);
+        setXYText(550,1200);
+        timerHandler.postDelayed(updateTimerThread, 10);
+        Toast.makeText(getContext(),"Bonjour", Toast.LENGTH_SHORT).show();
+    }
+
+    Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timerHandler.postDelayed(this, 1000);
+            cpt++;
+            invalidate(); // appel de onDraw pour redessiner
+        }
+    };
+
+    public void setXYText (float x, float y){
+        xText = x;
+        yText = y;
     }
 
     @Override
@@ -33,6 +59,20 @@ public class ToGraphic extends View {
         p.setTextAlign(android.graphics.Paint.Align.CENTER);
         /*dessiner le texte en positionnant son origine au centre du View */
         String texte = "Bonjour MONDE";
-        canvas.drawText(texte, getWidth()/2, getHeight()/2, p);
+        canvas.drawText(texte, xText, yText, p);
+        String scpt= String.valueOf(cpt);
+        canvas.drawText(scpt, xText, yText+100, p);
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.jack_o_lantern);
+        b = Bitmap.createBitmap(b,0,0,310,310);
+        canvas.drawBitmap(b, 380,450,p);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        xText = event.getX();
+        yText = event.getY();
+        invalidate();
+        return false;
+    }
+
 }
